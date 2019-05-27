44c8b6161db8a5b7f3352548ecbf033a4450fc98
Andy <anrao3@buffalo.edu>
2019-05-27 13:08:03 -0400

    Updates

---
+++ precache-manifest.b3975267594c0acac17fc5691cae0364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743546adacaaad8a862fb210753f0314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a6d134cbaa1d80bccc",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "107c7a7f4e1c3a37b261",
+    "url": "/static/css/main.a48658ee.chunk.css"
+  },
+  {
+    "revision": "a0a6d134cbaa1d80bccc",
+    "url": "/static/js/2.098b66e1.chunk.js"
+  },
+  {
+    "revision": "107c7a7f4e1c3a37b261",
+    "url": "/static/js/main.e9bc85ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
